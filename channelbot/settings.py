8d9f63f3922e66e060414e228253d64f799e36c2
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-06-20 15:27:14 +0300

    Fix settings

---
+++ channelbot/settings.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from os import environ
+import gettext
+
+
+_ = lambda x: x
+
+load_dotenv()
+
+TOKEN: str = environ['BOT_TOKEN']
+
+HOST: str = environ['HOST']
+WEBHOOK_PATH: str = f'bot/{TOKEN}'
+WEB_SERVER_URL: str = environ['WEB_SERVER_URL']
+WEBHOOK_URL: str = f'{WEB_SERVER_URL}{"/" if not WEB_SERVER_URL.endswith("/") else ""}{WEBHOOK_PATH}'
+
+REDIS_PORT = int(environ['REDIS_PORT'])
+REDIS_PASSWORD: str = environ['REDIS_PASSWORD']
+
+CHANNEL_ID = int(environ['CHANNEL_ID'])
+GROUP_ID = int(environ['GROUP_ID'])
+
+TIMEOUT = int(environ.get('TIMEOUT', 10))
