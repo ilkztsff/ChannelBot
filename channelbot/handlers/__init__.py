e34a8304b38909ca813fc1d1cbb3b67c182180fa
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-06-21 18:42:54 +0300

    Add routers

---
+++ channelbot/handlers/__init__.py
@@ -0,0 +1,4 @@
+from channelbot.handlers.admin import router as admin_rt
+from channelbot.handlers.channel import router as channel_rt
+from channelbot.handlers.group import router as group_rt
+from channelbot.handlers.private import router as private_rt
