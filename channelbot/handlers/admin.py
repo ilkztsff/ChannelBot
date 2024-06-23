d4642f2ee9a4f430ae8439a2864b8fa196726b27
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-06-23 15:31:37 +0300

    Add start handler

--- channelbot/handlers/admin.py
+++ channelbot/handlers/admin.py
@@ -1,4 +1,12 @@
-from aiogram import Router
+from aiogram import Router, types
+from aiogram.filters.command import Command
+
+from channelbot.settings import _
 
 
 router = Router()
+
+
+@router.message(Command('start'))
+async def start(msg: types.Message):
+    await msg.reply(_('Hello! Here you can anonymously communicate with your followers. Just choose one below to start chatting. If this list is empty, nobody is texting you'))
